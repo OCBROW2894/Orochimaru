8e4b552bdabe22bbc38d97a65e0ac77c1efe6b39
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-05-22 11:34:45 +0300

    Sum and Dates

--- Fun/New folder/Hello/main.py
+++ Fun/New folder/Hello/main.py
@@ -17,3 +17,8 @@ fire = '🔥'
 
 # Put code to run under here
 print(f'Hello {world}')
+print(f'Welcome to {python}')
+print(f'{python} is good at maths!')
+print(f'{2*4}')
+print(f'{4**2}') #** - to the power
+print(f'The Date and time is {datetime.now()}')
