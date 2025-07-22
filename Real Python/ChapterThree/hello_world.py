c0732f2ce4fea1e9f9b95b222907f40e9950de00
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-07-22 15:57:20 +0300

    Hello,World my Style

---
+++ Real Python/ChapterThree/hello_world.py
@@ -0,0 +1,6 @@
+user_name=input("What's your name? ")
+print(f"Hello, {user_name}\n")
+print("Welcome to the Land of Sound, Created by Orochmaru The Legendary Sanin\n")
+print("The World Of Infinite Possibilities")
+print("The World Of Real Science")
+
