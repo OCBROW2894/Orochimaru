50be0d2d9d798bf71f5829c418aee35a74bf8718
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-13 00:34:28 +0300

    Question 3

--- Real Python/ChapterFour/Review_exercise_two.py
+++ Real Python/ChapterFour/Review_exercise_two.py
@@ -7,3 +7,9 @@ first_name ="Rick"
 last_name="Morty"
 show_name= first_name +" & "+ last_name
 print(show_name)
+
+#Question 3
+first_name= "Naruto"
+last_name="Uzumaki"
+full_name=first_name+ " "+ last_name
+print(full_name)
