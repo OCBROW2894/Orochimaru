1eab4a7e617299a3390a4e155d869b07bca4ec17
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-13 00:16:30 +0300

    Slice the pie

---
+++ Real Python/ChapterFour/Slicing.py
@@ -0,0 +1,11 @@
+#Can You find the Hidden hummor
+flavor = "Apple Pie"
+print (flavor)
+flavor[0:5] #Start from Index zero to Index 4(Gives me the First 5 Characters)
+flavor[:3]
+flavor[5:]
+flavor[:]
+flavor[13:15]
+flavor[-9:-6]
+flavor[-6:-9]
+
---
+++ Real Python/ChapterFour/Slicing_Practice.py
@@ -0,0 +1,11 @@
+#Can You find the Hidden hummor
+flavor = "Apple Pie"
+print (flavor)
+print(flavor[0:5]) #Start from Index zero to Index 4(Gives me the First 5 Characters)
+print(flavor[:3])
+print(flavor[5:])
+print(flavor[:])
+print(flavor[13:15])
+print(flavor[-9:-6])
+print(flavor[-6:-9])
+
