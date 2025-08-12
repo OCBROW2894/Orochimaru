c80e1fa5635e34f1a7da11d29cebaa2e43618486
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-13 00:41:50 +0300

    Question 4 & end of Review exercise 2(4.2)

--- Real Python/ChapterFour/Review_exercise_two.py
+++ Real Python/ChapterFour/Review_exercise_two.py
@@ -1,15 +1,22 @@
-#Question 1
+#Question 1 : Create a string and print its length using the len() function.
 proffesion= "Software Engineer"
 print(len(proffesion))
 
-#Question 2
+#Question 2 : Create two strings, concatenate them, and print the resulting string.
 first_name ="Rick"
 last_name="Morty"
-show_name= first_name +" & "+ last_name
+show_name= first_name + last_name
 print(show_name)
 
-#Question 3
+'''Question 3 : Create two strings and use concatenation to add a space in between them.
+Then print the result. '''
 first_name= "Naruto"
 last_name="Uzumaki"
 full_name=first_name+ " "+ last_name
 print(full_name)
+
+'''Question 4 : Print the string "zing" by using slice notation on the string
+ "bazinga" to specify the correct range of characters. '''
+name="bazinga"
+solution=name[2:6]
+print(solution)
