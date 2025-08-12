75fe39173eebc722563c04f3b462205381a99310
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-12 17:55:00 +0300

    Review exercise one (4.1)

---
+++ Real Python/ChapterFour/Riview_exercise_one.py
@@ -0,0 +1,13 @@
+#Review Exercise
+#Question 1
+print('"Oden Sama!", the Samurais cried')
+#Question 2
+print("Kaido's Dragon's breath was so cool when said in Japanese")
+#Question 3
+print(""" Naruto Uzumaki & Hinata Hyuga
+        Sasuke Uchiha & Sakura Yamanaka """)
+#Question 4
+print("My, \
+ goal is to never regret about any decisions \
+ I made when I had the power of my youth, when too old \
+ and ready to pass the Batton")
