312333741709936eba0fc60915f0d87dd5225ef2
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-13 15:20:14 +0300

    Question 1

---
+++ Real Python/ChapterFour/Review_exercise_three.py
@@ -0,0 +1,11 @@
+'''Question 1 : Writeascriptthatconvertsthefollowingstringstolowercase: "An
+imals", "Badger", "Honey Bee", "Honeybadger". Print each lowercase
+ string on a separate line. '''
+name_one="Animals".lower()
+name_two="Badger".lower()
+name_three="Honey Bee".lower()
+name_four="Honeybadger".lower()
+print(name_one)
+print(name_two)
+print(name_three)
+print(name_four)
