45147c0ea71511d5a238fe11f605e26508bb9cf9
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-13 16:27:59 +0300

    Question 3 and end of Review exercise (4.4)

--- Real Python/ChapterFour/Review_exercise_four.py
+++ Real Python/ChapterFour/Review_exercise_four.py
@@ -1,10 +1,15 @@
-'''Question 1 : Write a script that takes input from the user and displays that in
-put back. '''
-name=input("Hello, what's your name? ")
-print("Greetings! " + name)
+##'''Question 1 : Write a script that takes input from the user and displays that in
+##put back. '''
+##name=input("Hello, what's your name? ")
+##print("Greetings! " + name)
+##
+##'''Question 2 : Write a script that takes input from the user and displays the input
+## in lowercase. '''
+##prompt="What is the name of the city or town you currently in? "
+##city_name=input(prompt).lower()
+##print(city_name)
 
-'''Question 2 : Write a script that takes input from the user and displays the input
- in lowercase. '''
-prompt="What is the name of the city or town you currently in? "
-city_name=input(prompt).lower()
-print(city_name)
+#Question 3 : Write a script that takes input from the user and displays the number of characters inputted.
+name=input("Enter your name to count the number of characters it contains?(1 name) ")
+length=len(name)
+print(length)
