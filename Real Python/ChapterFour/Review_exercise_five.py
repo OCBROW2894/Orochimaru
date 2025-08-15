457eb4013f2dee86ee728cf8b49484f6f95471f7
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-15 20:49:43 +0300

    Question 2

--- Real Python/ChapterFour/Review_exercise_five.py
+++ Real Python/ChapterFour/Review_exercise_five.py
@@ -2,9 +2,17 @@
  an actual integer object using int(). Test that your new object is a number by multiplying
  it by another number and displaying the result. '''
 
-eggs=input("Enter the number of eggs you want to buy: ")
-price=18
+##eggs=input("Enter the number of eggs you want to buy: ")
+##price=18
+##
+##no_eggs=int(eggs)
+##total_cost=no_eggs*price
+##print(total_cost)
 
-no_eggs=int(eggs)
+#Question 2 : Repeatthepreviousexercise, but use a floating-point number and float().
+eggs=input("Enter the number of eggs you want to buy: ")
+price=18.00
+
+no_eggs=float(eggs)
 total_cost=no_eggs*price
 print(total_cost)
