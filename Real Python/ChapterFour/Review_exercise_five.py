2b724d3e41d1c5abaa84ab89039f02b663d66117
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-16 22:22:01 +0300

    Question 3

--- Real Python/ChapterFour/Review_exercise_five.py
+++ Real Python/ChapterFour/Review_exercise_five.py
@@ -9,10 +9,18 @@
 ##total_cost=no_eggs*price
 ##print(total_cost)
 
-#Question 2 : Repeatthepreviousexercise, but use a floating-point number and float().
-eggs=input("Enter the number of eggs you want to buy: ")
-price=18.00
+'''Question 2 : Repeatthepreviousexercise, but use a floating-point number and float(). '''
+##eggs=input("Enter the number of eggs you want to buy: ")
+##price=18.00
+##
+##no_eggs=float(eggs)
+##total_cost=no_eggs*price
+##print(total_cost)
 
-no_eggs=float(eggs)
-total_cost=no_eggs*price
-print(total_cost)
+'''Question 3 : Createastringobjectandanintegerobject,thendisplaythemside
+by-side with a single print statement by using the str() function. '''
+
+boo_age=20
+bum_age="24"
+difference=int(bum_age)-boo_age
+print("My Boo is "+ str(difference) +" years younger than me, she's "+ str(boo_age)+ " and I'm "+bum_age)
