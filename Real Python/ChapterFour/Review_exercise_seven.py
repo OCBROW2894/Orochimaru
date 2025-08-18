85ec73ef7e15dd7244b898a53354445c6975a873
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-18 18:48:31 +0300

    Question 2

--- Real Python/ChapterFour/Review_exercise_seven.py
+++ Real Python/ChapterFour/Review_exercise_seven.py
@@ -1,3 +1,9 @@
 '''Question 1 : In one line of code, display the result of trying to .find() the sub-
 string "a" in the string "AAA". The result should be-1.'''
-print("AAA".find("a"))
+##print("AAA".find("a"))
+
+'''Question 2 : Replaceeveryoccurrenceofthecharacter"s"with"x"inthestring
+ "Somebody said something to Samantha.". '''
+message= "Somebody said something to Samantha."
+message=message.replace("s","x")
+print(message)
