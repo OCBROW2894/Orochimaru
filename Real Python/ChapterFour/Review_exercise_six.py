c3e4c9086f10c60329fc32239894006844fbc92e
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-18 18:26:02 +0300

    Question 1

---
+++ Real Python/ChapterFour/Review_exercise_six.py
@@ -0,0 +1,8 @@
+'''Create a float object named weight with the value 0.2, and create
+ a string object named animal with the value "newt". Then use these
+ objects to print the following string using only string concatenation:
+ 0.2 kg is the weight of the newt. '''
+
+weight=float(0.2)
+animal= "newt"
+print(str(0.2)+ " kg is the weight of the " + animal + ".")
