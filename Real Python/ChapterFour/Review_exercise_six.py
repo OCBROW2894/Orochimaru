6ea83713d1ab59b7c5a67f6f21f67b8796dabad7
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-18 18:30:33 +0300

    Question 2

--- Real Python/ChapterFour/Review_exercise_six.py
+++ Real Python/ChapterFour/Review_exercise_six.py
@@ -1,8 +1,14 @@
-'''Create a float object named weight with the value 0.2, and create
+'''Question 1 : Create a float object named weight with the value 0.2, and create
  a string object named animal with the value "newt". Then use these
  objects to print the following string using only string concatenation:
  0.2 kg is the weight of the newt. '''
 
-weight=float(0.2)
-animal= "newt"
-print(str(0.2)+ " kg is the weight of the " + animal + ".")
+##weight=float(0.2)
+##animal= "newt"
+##print(str(0.2)+ " kg is the weight of the " + animal + ".")
+
+'''Question 2 : Display the same string by using the .format() method and empty
+ {} place-holders.'''
+weight=0.2
+animal="newt"
+print("{} kg is the weight of the {}.".format(weight,animal))
