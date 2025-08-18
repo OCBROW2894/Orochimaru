27ac9362df053ff7fddc3032e9190a0ab36a8b9b
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-18 18:14:07 +0300

    F-strings

--- Real Python/ChapterFour/string_interpolation.py
+++ Real Python/ChapterFour/string_interpolation.py
@@ -1,8 +1,11 @@
-#String interpolation
-n = int(3)
-m = int(4)
-s = n*m
-print(str(n) +" times "+ str(m) + " is " + str(n*m))
-print(str(n), " times ", str(m) , " is " , str(n*m))
+###String interpolation
+##n = int(3)
+##m = int(4)
+##s = n*m
+##print(str(n) +" times "+ str(m) + " is " + str(n*m))
+##print(str(n), " times ", str(m) , " is " , str(n*m))
 
-
+n = 3
+m = 4
+print(f"{n} times {m} is {n*m}") #f-strings are only available in Python version 3.6 and above.
+print("{} times {} is {}".format(n,m,n*m))#For earlier versions
