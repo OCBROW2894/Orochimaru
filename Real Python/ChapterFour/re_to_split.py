1c7b6eb47442b9e58857288d687fcc7fa3652dcf
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-22 07:49:10 +0300

    Using the special caharacters

---
+++ Real Python/ChapterFour/re_to_split.py
@@ -0,0 +1,20 @@
+'''
+* Use re.split() for Advanced String Splitting
+ Here’s a list of the regex constructs that you used to make this split happen:
+
+\s: Matches any whitespace character.
+*: Matches zero or more occurrences of the preceding.
+(?:): Creates an alternation group, for example (?:abc|def), that matches any of the patterns abc or def in their entirety. In your specific example, this allows you to treat AND as a single delimiter to split on.
+[]: Creates a character set that matches any one of the characters inside the square brackets.
++: Matches one or more occurrences of the preceding.
+
+NOTE:This Code only works in the interactive screen
+
+'''
+
+import re
+
+shopping_list = "Apple  :::::3:Orange  |  2|||Lemon --1 AND Date :: 10"
+pattern = r"\s*(?:[:|\-]+|AND)\s*"
+
+re.split(pattern, shopping_list)
--- Real Python/ChapterFour/spliting.py
+++ Real Python/ChapterFour/spliting.py
@@ -20,7 +20,17 @@
 ##print(ex)
 ##print(color)
 
-#Replacing delimeters before splitting
-data = "Apple:Orange|Lemon-Date"
-data1= data.replace(":","|").replace("-","|").split(sep="|")
-print(data1)
+###Replacing delimeters before splitting
+##data = "Apple:Orange|Lemon-Date"
+##data1= data.replace(":","|").replace("-","|").split(sep="|")
+##print(data1)
+
+
+log_line = "2025-01-15 08:45:23 INFO User logged in from IP 10.0.1.1"
+
+date, time, log_level, message = log_line.split(maxsplit=3)
+
+print(f"Date: {date}")
+print(f"Time: {time}")
+print(f"Log Level: {log_level}")
+print(f"Message: {message}")
