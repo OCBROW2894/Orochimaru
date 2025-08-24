d7e36ea080f110187922157210493ec37134bef5
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-24 10:29:38 +0300

    Question 2

--- Real Python/ChapterFive/review_exercise_one.py
+++ Real Python/ChapterFive/review_exercise_one.py
@@ -2,7 +2,13 @@
  num1 and num2 should be assigned the integer literal 25,000,000,
  one written with underscored and one without. Print num1 and num2
  on two separate lines. """
-num1 = 25000000
-num2 = 25_000_000
+##num1 = 25000000
+##num2 = 25_000_000
+##
+##print(f" {num1} \n {num2} ") 
 
-print(f" {num1} \n {num2} ") 
+'''Question 2 : Write a script that assigns the floating-point literal 175000.0 to the
+ variable num using exponential notation, and then prints num in the
+ interactive window. '''
+num = 175e3
+print(num)
