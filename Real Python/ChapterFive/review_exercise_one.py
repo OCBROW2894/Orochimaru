ea567bfc8464226df6424c8dfd58fa5081ac09be
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-24 09:00:12 +0300

    Question 1

---
+++ Real Python/ChapterFive/review_exercise_one.py
@@ -0,0 +1,8 @@
+"""Question 1 : Write a script that creates the two variables, num1 and num2. Both
+ num1 and num2 should be assigned the integer literal 25,000,000,
+ one written with underscored and one without. Print num1 and num2
+ on two separate lines. """
+num1 = 25000000
+num2 = 25_000_000
+
+print(f" {num1} \n {num2} ") 
