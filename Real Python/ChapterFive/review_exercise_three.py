2c89df0690db913d0269529aa4fcd06e515087f4
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-26 16:33:51 +0300

    Question 1

---
+++ Real Python/ChapterFive/review_exercise_three.py
@@ -0,0 +1,8 @@
+'''Question 1 : Print the result of the calculation 3 ** .125 as a fixed-pointnumber
+ with three decimal places.
+'''
+base = 3
+power = .125
+answer = pow(base,power)
+
+print(f"{base} to the power of {power} is {answer:.3f}")
