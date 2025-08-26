c6cce1a0bffa4bd98e4fee8d98a666a6ccb63229
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-26 15:01:06 +0300

    Question 2

--- Real Python/ChapterFive/review_exercise_two.py
+++ Real Python/ChapterFive/review_exercise_two.py
@@ -4,8 +4,19 @@
  Enter a number: 5.432
  5.432 rounded to 2 decimal places is 5.43
  '''
+##n = float(input("Enter a number: "))
+##
+##rounded_n = round(n,2)
+##
+##print(f"{n} rounded to 2 decimal places is {rounded_n}")
+
+'''Question 2 : Write a script that asks the user to input a number and then dis
+ plays the absolute value of that number. When run, your program
+ should look like this:
+ Enter a number:-10
+ The absolute value of-10 is 10.0
+ '''
 n = float(input("Enter a number: "))
+absolute_n = abs(n)
 
-rounded_n = round(n,2)
-
-print(f"{n} rounded to 2 decimal places is {rounded_n}")
+print(f"The absolute value of {n} is {absolute_n}")
