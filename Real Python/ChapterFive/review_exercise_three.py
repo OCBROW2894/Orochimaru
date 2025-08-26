6b61353d67ecd440d33bd6512b34cf3864dcea4d
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-26 16:41:53 +0300

    Question 2

--- Real Python/ChapterFive/review_exercise_three.py
+++ Real Python/ChapterFive/review_exercise_three.py
@@ -1,8 +1,14 @@
 '''Question 1 : Print the result of the calculation 3 ** .125 as a fixed-pointnumber
  with three decimal places.
 '''
-base = 3
-power = .125
-answer = pow(base,power)
+##base = 3
+##power = .125
+##answer = pow(base,power)
+##
+##print(f"{base} to the power of {power} is {answer:.3f}")
 
-print(f"{base} to the power of {power} is {answer:.3f}")
+'''Question 2 : Print the number 150000 as currency, with the thousands grouped
+ with commas. Currency should be displayed with two decimal places.
+'''
+n = 150000
+print(f"{n:,.2f}")
