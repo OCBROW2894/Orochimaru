af039851e33de59f38733b452276c66ccab74f9f
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-26 15:24:25 +0300

    Question 3

--- Real Python/ChapterFive/review_exercise_two.py
+++ Real Python/ChapterFive/review_exercise_two.py
@@ -16,7 +16,25 @@
  Enter a number:-10
  The absolute value of-10 is 10.0
  '''
-n = float(input("Enter a number: "))
-absolute_n = abs(n)
+##n = float(input("Enter a number: "))
+##absolute_n = abs(n)
+##
+##print(f"The absolute value of {n} is {absolute_n}")
 
-print(f"The absolute value of {n} is {absolute_n}")
+'''Question 3 : Write a script that asks the user to input two numbers by using the
+ input() function twice, then display whether or not the difference
+ between those two number is an integer. When run,your program
+ should look like this:
+ Enter a number: 1.5
+ Enter another number: .5
+ The difference between 1.5 and .5 is an integer? True!
+ If the user inputs two numbers whose difference is not integral,
+ the output should look like this:
+ Enter a number: 1.5
+ Enter another number: 1.0
+ The difference between 1.5 and 1.0 is an integer? False!
+ '''
+n = float(input("Enter a number: "))
+m = float(input("Enter another number: "))
+answer = (n-m).is_integer()
+print(f"The difference between {n} and {m} is an integer? {answer}!")
