eec8928e7401d6e65faf3b4f2f56397633378412
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-28 15:33:03 +0300

    Code order corrected

--- Real Python/ChapterSix/function_intro.py
+++ Real Python/ChapterSix/function_intro.py
@@ -1,9 +1,17 @@
 """You must define the
  function before you call it."""
 
-num = multiply(28,94)
-print(num)
+##num = multiply(28,94)
+##print(num)
+##
+##def multiply(x,y):
+##    product = x*y
+##    return product
+
 
 def multiply(x,y):
     product = x*y
     return product
+
+num = multiply(28,94)
+print(num)
