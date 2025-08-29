f070b02c6ceb31cbc9fed00ca0f97e23ba6055db
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-08-29 17:43:57 +0300

    Did it the right way(Investment Tracker)

--- Real Python/ChapterSix/invest.py
+++ Real Python/ChapterSix/invest.py
@@ -1,7 +1,13 @@
-
+"""
+Challenge: Track Your Investments
+"""
 def invest(amount, rate, years):
-    interest = (amount * (rate/100))
-    amount = amount + interest
+    start = 1
+    while start <= years:
+        start = start + 1
+        interest = (amount * (rate/100))
+        amount = amount + interest
+        print(f"year {start - 1}: ${amount:,.2f}")
     return amount
 
 
@@ -9,10 +15,4 @@ balance = float(input("Enter your capital: "))
 rate_p_a = float(input("Enter the annual percentage rate: "))
 period = float(input("Number of Cycles: "))
 
-start = 1
-
-while start <= period :
-    start = start + 1
-    balance = invest(balance, rate_p_a, period)
-    print(f"year {start - 1}: ${balance:,.2f}")
-
+invest(balance, rate_p_a, period)
