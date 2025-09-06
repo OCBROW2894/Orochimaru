8671ac36c32a0f0416802b870c9db81984a15da6
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-09-06 08:58:37 +0300

    Killed the bug using the debugger

--- Real Python/ChapterSeven/debug_the_code.py
+++ Real Python/ChapterSeven/debug_the_code.py
@@ -5,7 +5,7 @@
 def add_underscores(word):
     new_word = "_"
     for i in range(0, len(word)):
-        new_word = word[i] + "_"
+        new_word = new_word + word[i] + "_"
     return new_word
 
 phrase = "hello"
