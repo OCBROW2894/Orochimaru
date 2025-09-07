7cd607d77af34521111932be5f769a97f75b7c0e
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-09-07 08:47:01 +0300

    Challenge 8.4 Completed

--- Real Python/ChapterEight/factors.py
+++ Real Python/ChapterEight/factors.py
@@ -1,4 +1,14 @@
 """
  Challenge: FindtheFactors of a Number
 """
+def factors(num):
+    for n in range(1, num + 1) :
+        factor = num % n
+        if factor == 0:
+            print(f"{n} is a factor of {num}")
+        n = n + 1
+    return factor
 
+number = int(input("Enter a positive integer: "))
+
+factors(number)
