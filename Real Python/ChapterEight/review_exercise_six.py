3f4874ead9444368e9aa1dd8cd87b9ec5cf073e0
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-09-10 06:21:59 +0300

    Question 2

--- Real Python/ChapterEight/review_exercise_six.py
+++ Real Python/ChapterEight/review_exercise_six.py
@@ -2,12 +2,30 @@
  simulate rolling a fair die by returning a random integer between
  1 and 6.
 """
+##import random
+##
+##def roll():
+##    n = random.randint(1,6)
+##    print(n)
+##    return n
+##
+##roll()
+
+"""Question 2 : Write a script that simulates 10,000 rolls of a fair die and displays
+ the average number rolled.
+"""
 import random
 
 def roll():
     n = random.randint(1,6)
-    print(n)
     return n
 
-roll()
+num_rolls = 10_000
+total = 0
 
+for trial in range(num_rolls):
+    total = total + roll()
+
+avg_roll = total / num_rolls
+
+print(f"The average result of {num_rolls} rolls is {avg_roll:.3f}")
