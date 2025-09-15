fd727bd55e9e99fd3b3471920a174fdd96c1a447
Bryian Owuor Ochieng <93646974+OCBROW2894@users.noreply.github.com>
2025-09-15 23:24:09 +0300

    Update GettingDataType.py

--- GettingDataType.py
+++ GettingDataType.py
@@ -2,7 +2,7 @@
 I think this is a very important to know how to get 
 the type of data of a variable for U to know how to really use it 
 or Know if it's the cause of the problem you going through (Logical Errors)
-
+Funny
 """
 x = 2894;
 y = "Bryian";
